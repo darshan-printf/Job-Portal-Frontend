32c76b2a2df321ab30a0888e43997d5eafd4b507
darshan-printf <darshan.talaviya@egrambook.com>
2025-09-20 11:58:13 +0530

    code optimaiton

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,34 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { KeyRound, UserLock } from "lucide-react";
-
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Login() {
-  const apiUrl = process.env.REACT_APP_API_URL; // Get the API URL from the .env file
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+  const [showPassword, setShowPassword] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   const navigate = useNavigate();
-  const [loaded, setLoaded] = useState(false); // Button loading effect state
+  const apiUrl = process.env.REACT_APP_API_URL;
   const Env = process.env;
 
   useEffect(() => {
     const isLogin = localStorage.getItem("isLogin");
-
     if (isLogin === "true") {
-      console.log("User is logged in, clearing localStorage...");
       localStorage.clear();
     }
+    // eslint-disable-next-line
   }, []);
 
-  // Function to handle form submission
   const handleSubmit = (e) => {
     setLoaded(true);
-    e.preventDefault(); // Prevent default form submission
-
-    // Prepare data for login request
+    e.preventDefault();
     const data = JSON.stringify({
-      username: email, // Assuming 'userName' is the same as email
+      username: email,
       password: password,
     });
-
-    // Axios configuration for login request
     const config = {
       method: "post",
       maxBodyLength: Infinity,
@@ -45,28 +38,22 @@ export default function Login() {
       },
       data: data,
     };
-
-    // Make the API request
     axios
       .request(config)
       .then((response) => {
         setLoaded(false);
         if (response?.data) {
-          console.log(response.data, "resposn");
-          // Save tokens and user info to localStorage
           localStorage.setItem("token", response.data.token);
           localStorage.setItem("role", response.data.role);
           localStorage.setItem("isLogin", true);
           localStorage.setItem("id", response.data.id);
           const duration = 1500000;
-
           setTimeout(() => {
             localStorage.setItem("isLogin", false);
           }, duration);
-
-          if (response.data.role == "admin") {
+          if (response.data.role === "admin") {
             navigate("/admin/dashboard");
-          } else if (response.data.role == "user") {
+          } else if (response.data.role === "user") {
             navigate("/admin/userdashboard");
           } else {
             navigate("/");
@@ -89,105 +76,97 @@ export default function Login() {
         }
       });
   };
-
   return (
-    <>
-      <div className="star-field">
-        <div className="login-page-bg bg" id="page-bg">
-          <div className="login-box">
-            <div className="card card-outline card-primary pb-4">
-              <div class="text-center mt-2">
-                <img
-                  class="profile-user-img-com img-fluid border border-0"
-                  src={Env.REACT_APP_PROJECT_ICON}
-                  alt="User profile picture"
-                />
-
-                <h3 className="mt-2">{Env.REACT_APP_PROJECT_NAME}</h3>
-              </div>
-              <div className="card-body pt-0 ">
-                <p className="login-box-msg">
-                  {Env.REACT_APP_PROJECT_NAME} Sign In
-                </p>
-                <form onSubmit={handleSubmit}>
-                     <UserLock /> <label>Username</label>
-                  <div className="input-group mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Enter Your Username"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)} // Update email state
-                      required
-                    />
-                    <div className="input-group-append">
-                      <div className="input-group-text">
-                        <span className="fas fa-user"></span>
-                      </div>
+    <div className="star-field">
+      <div className="login-page-bg bg" id="page-bg">
+        <div className="login-box">
+          <div className="card card-outline card-primary pb-4">
+            <div class="text-center mt-2">
+              <img
+                class="profile-user-img-com img-fluid border border-0"
+                src={Env.REACT_APP_PROJECT_ICON}
+                alt="logo"
+              />
+              <h3 className="mt-2">{Env.REACT_APP_PROJECT_NAME}</h3>
+            </div>
+            <div className="card-body pt-0 ">
+              <p className="login-box-msg">
+                {Env.REACT_APP_PROJECT_NAME} Sign In
+              </p>
+              <form onSubmit={handleSubmit}>
+                <UserLock /> <label>Username</label>
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Your Username"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-user"></span>
                     </div>
                   </div>
-                   <KeyRound /> <label>Password</label>
-                  <div className="input-group mb-3">
-                    <input
-                      type={showPassword ? "text" : "password"} // Toggle input type
-                      className="form-control"
-                      placeholder="Enter Your Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)} // Update password state
-                      required
-                    />
-                    <div className="input-group-append">
-                      <button
-                        type="button"
-                        className="input-group-text"
-                        onClick={() => setShowPassword(!showPassword)} // Toggle password visibility
-                        style={{ cursor: "pointer" }}
-                      >
+                </div>
+                <KeyRound /> <label>Password</label>
+                <div className="input-group mb-3">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className="form-control"
+                    placeholder="Enter Your Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <div className="input-group-append">
+                    <button
+                      type="button"
+                      className="input-group-text"
+                      onClick={() => setShowPassword(!showPassword)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <span
+                        className={
+                          showPassword ? "fas fa-eye-slash" : "fas fa-eye"
+                        }
+                      ></span>
+                    </button>
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <div className="col-12">
+                    <button
+                      className="btn btn-primary btn-block"
+                      disabled={loaded}
+                    >
+                      {loaded ? (
                         <span
-                          className={
-                            showPassword ? "fas fa-eye-slash" : "fas fa-eye"
-                          }
+                          className="spinner-border spinner-border-sm"
+                          role="status"
+                          aria-hidden="true"
                         ></span>
-                      </button>
-                    </div>
+                      ) : (
+                        <>Login</>
+                      )}
+                    </button>
                   </div>
-                  <div className="row justify-content-center">
-                    <div className="col-12">
-                      <button
-                        className="btn btn-primary btn-block"
-                        disabled={loaded}
-                      >
-                        {loaded ? (
-                          <>
-                            {" "}
-                            <span
-                              className="spinner-border spinner-border-sm"
-                              role="status"
-                              aria-hidden="true"
-                            ></span>{" "}
-                            Please wait...
-                          </>
-                        ) : (
-                          <>Login</>
-                        )}
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className="text-center mt-3">
-                <p className="text-center mb-1">
-                  New Registration <Link to={"/admin/registration"}>Click</Link>
-                </p>
-                <p className="text-center mb-0">
-                  Designed by <Link>{Env.REACT_APP_DEVLOPER_NAME}</Link>
-                </p>
-              </div>
+                </div>
+              </form>
+            </div>
+            <div className="text-center mt-0">
+              <p className="text-center mb-1">
+                New Registration <Link to={"/admin/registration"}>Click</Link>
+              </p>
+              <p className="text-center mb-0">
+                Designed by <Link>{Env.REACT_APP_DEVLOPER_NAME}</Link>
+              </p>
             </div>
           </div>
         </div>
       </div>
-      <ToastContainer />
-    </>
+      <ToastContainer style={{ width: "auto" }} />
+    </div>
   );
 }
